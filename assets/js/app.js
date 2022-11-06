d79bf5c3fc737cbaf052ae03374376a912cadd4a
Johhan Urrutia <johhanurrutia@MacBook-Pro-de-Johhan.local>
2022-11-06 18:43:14 -0300

    Correccion v1.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,21 +7,21 @@ var validacionFinal = document.getElementById("validacionFinal")
 botonVerificar.addEventListener("click", () => {
 
     if (isNaN(parseInt(inputUno.value))){
-        inputUno = 1
+        inputUno = 0
     } else if (parseInt(inputUno.value) != NaN){
         inputUno = parseInt(inputUno.value)
         console.log("Entro al else if 1")
     }
 
     if (isNaN(parseInt(inputDos.value))){
-        inputDos = 1
+        inputDos = 0
     } else if (parseInt(inputDos.value) != NaN){
         inputDos = parseInt(inputDos.value)
         console.log("Entro al else if 2")
     }
 
     if (isNaN(parseInt(inputTres.value))){
-        inputTres = 1
+        inputTres = 0
     } else if (parseInt(inputTres.value) != NaN){
         inputTres = parseInt(inputTres.value)
         console.log("Entro al else if 3")
@@ -31,7 +31,9 @@ botonVerificar.addEventListener("click", () => {
 
     if (sumaInt < 11){
         validacionFinal.innerHTML = `LLevas ${sumaInt} stickers`
+        setTimeout(function(){ location. reload() }, 2000);
     } else{
         validacionFinal.innerHTML = `LLevas demasiados stickers`
+        setTimeout(function(){ location. reload() }, 2000);
     }
 })
